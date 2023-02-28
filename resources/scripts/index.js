2c77ecf67db7b774e93a9a60b069a627f169de45
farzana-moury <w0737626@myscc.ca>
2023-02-27 20:56:09 -0500

    level one: add display logic for odd dice out

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,19 +1,32 @@
 // important variables and constants
-let oddDiceNumber, commonDiceNumber, dicePerColumn, randomPosition;
-let gameScreen = [], totalDice;
-const dicePerRow = 6;
+let oddDiceNumber, commonDiceNumber, totalRows, randomPosition;
+let gameScreen = [], totalDice, totalCommonDice;
+const totalColumns = 6;
 
 function levelOne(){
+    totalRows = 1;
+    gameScreen.length = totalColumns * totalRows;
     oddDiceNumber = Math.floor(Math.random() * 6) + 1;
     commonDiceNumber = Math.floor(Math.random() * 6) + 1;
 
+    // check to ensure the two dice numbers are always different 
     if(commonDiceNumber === oddDiceNumber){
         commonDiceNumber = Math.floor(Math.random() * 6) + 1;
     }else{
         commonDiceNumber = commonDiceNumber;
     }
 
-    console.log(oddDiceNumber, commonDiceNumber);
+    // console.log(oddDiceNumber, commonDiceNumber);
+
+    //place in all the common dice first
+    for(let i = 0; i < gameScreen.length; i++){
+        gameScreen[i] = commonDiceNumber;
+    }
+    //then place the odd dice in a random position
+    randomPosition = Math.floor(Math.random() * 5);
+    gameScreen[randomPosition] = oddDiceNumber;
+
+    console.log(gameScreen);
 }
 
 levelOne();
