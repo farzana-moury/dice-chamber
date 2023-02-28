80bbf4e3b29a317fdd492d6606e4c659fc97274a
farzana-moury <w0737626@myscc.ca>
2023-02-27 22:16:59 -0500

    level one: display dice according to logic

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,47 +1,49 @@
 // important variables and constants
 let oddDiceNumber, commonDiceNumber, totalRows, randomPosition;
-let gameScreenArray = [], totalDice, totalCommonDice;
+let diceArray = [], totalDice, totalCommonDice;
 let winRound, winLevel;
 const totalColumns = 6;
 
 // html elements
-let oddDice = document.createElement('img');
-let commonDice = document.createElement('img');
 let gameScreen = document.getElementById('game-screen');
-
 
 function levelOne(){
     totalRows = 1;
-    gameScreenArray.length = totalColumns * totalRows;
+    diceArray.length = totalColumns * totalRows;
 
     // set odd dice and common dice numbers
     oddDiceNumber = Math.floor(Math.random() * 6) + 1;
     commonDiceNumber = Math.floor(Math.random() * 6) + 1;
 
     // check to ensure the two dice numbers are always different 
-    if(commonDiceNumber === oddDiceNumber){
+    if (commonDiceNumber === oddDiceNumber){
         commonDiceNumber = Math.floor(Math.random() * 6) + 1;
-    }else{
+    } else{
         commonDiceNumber = commonDiceNumber;
     }
 
-    console.log(oddDiceNumber, commonDiceNumber);
-
-    //place in all the common dice first
-    for(let i = 0; i < gameScreenArray.length; i++){
-        gameScreenArray[i] = commonDiceNumber;
+    // place down all the common dice first
+    for (let i = 0; i < diceArray.length; i++) {
+        diceArray[i] = commonDiceNumber;
     }
-    //then place the odd dice in a random position
+    // then place the odd dice on a random position
     randomPosition = Math.floor(Math.random() * 5);
-    gameScreenArray[randomPosition] = oddDiceNumber;
+    diceArray[randomPosition] = oddDiceNumber;
 
-    // console.log(gameScreen);
+    // set odd dice and common dice images on the screen accordingly
+    for (const dice of diceArray) {
+        if(dice === oddDiceNumber){
+            let oddDice = document.createElement('img');
+            oddDice.src = `../images/dice-${oddDiceNumber}.png`;
+            gameScreen.appendChild(oddDice);
+        } else {
+            let commonDice = document.createElement('img');
+            commonDice.src = `../images/dice-${commonDiceNumber}.png`;
+            gameScreen.appendChild(commonDice);
+        }
+    }
 
-    // set odd dice and common dice images
-    oddDice.src = `../images/dice-${oddDiceNumber}.png`;
-    commonDice.src = `../images/dice-${commonDiceNumber}.png`;
-
-    gameScreen.append(oddDice, commonDice);
+    console.log(diceArray);
 }
 
 levelOne();
