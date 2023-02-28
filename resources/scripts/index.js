436333c3fd50647c36b7b958d64b7a2bbdcce7b2
farzana-moury <w0737626@myscc.ca>
2023-02-27 21:51:59 -0500

    level one: html dice images accordingly match with corresponding dice numbers

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,12 +1,20 @@
 // important variables and constants
 let oddDiceNumber, commonDiceNumber, totalRows, randomPosition;
-let gameScreen = [], totalDice, totalCommonDice;
+let gameScreenArray = [], totalDice, totalCommonDice;
 let winRound, winLevel;
 const totalColumns = 6;
 
+// html elements
+let oddDice = document.createElement('img');
+let commonDice = document.createElement('img');
+let gameScreen = document.getElementById('game-screen');
+
+
 function levelOne(){
     totalRows = 1;
-    gameScreen.length = totalColumns * totalRows;
+    gameScreenArray.length = totalColumns * totalRows;
+
+    // set odd dice and common dice numbers
     oddDiceNumber = Math.floor(Math.random() * 6) + 1;
     commonDiceNumber = Math.floor(Math.random() * 6) + 1;
 
@@ -17,17 +25,23 @@ function levelOne(){
         commonDiceNumber = commonDiceNumber;
     }
 
-    // console.log(oddDiceNumber, commonDiceNumber);
+    console.log(oddDiceNumber, commonDiceNumber);
 
     //place in all the common dice first
-    for(let i = 0; i < gameScreen.length; i++){
-        gameScreen[i] = commonDiceNumber;
+    for(let i = 0; i < gameScreenArray.length; i++){
+        gameScreenArray[i] = commonDiceNumber;
     }
     //then place the odd dice in a random position
     randomPosition = Math.floor(Math.random() * 5);
-    gameScreen[randomPosition] = oddDiceNumber;
+    gameScreenArray[randomPosition] = oddDiceNumber;
 
     // console.log(gameScreen);
+
+    // set odd dice and common dice images
+    oddDice.src = `../images/dice-${oddDiceNumber}.png`;
+    commonDice.src = `../images/dice-${commonDiceNumber}.png`;
+
+    gameScreen.append(oddDice, commonDice);
 }
 
 levelOne();
