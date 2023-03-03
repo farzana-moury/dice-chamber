29700628e19a602fbec6d27c55475d88c05a04a8
farzana-moury <w0737626@myscc.ca>
2023-03-02 21:01:04 -0500

    #1 level two: display expression using efficient loop

--- resources/scripts/levelTwo.js
+++ resources/scripts/levelTwo.js
@@ -7,7 +7,7 @@ let option1Answer = 0, option2Answer = 0, option3Answer = 0;
 let winRound = 1, winLevel;
 
 // html elements
-// let gameScreen = document.getElementById('game-screen');
+let gameScreen = document.getElementById('game-screen');
 
 // const equalSign = document.createElement("h1");
 // equalSign.innerText = "=";
@@ -15,46 +15,47 @@ let winRound = 1, winLevel;
 // const questionMark = document.createElement("h1");
 // questionMark.innerText = "?";
 
-function levelTwo(){
+function levelTwo() {
     numberOfOperands = 5;
-    
+
     // creating a random expression
-    for (let i = 0; i < numberOfOperands; i++){
+    for (let i = 0; i < numberOfOperands; i++) {
         let randomNum = Math.floor(Math.random() * 6) + 1;
         let randomOperator = Math.floor(Math.random() * (allOperators.length));
         expressionArray.push(randomNum);
         expressionArray.push(allOperators[randomOperator]);
     }
+    expressionArray[expressionArray.length - 1] = "="; // replacing the last operator to be equal operator
 
     // grabbing all the operators within the expression
-    for (let i = 1; i <= expressionArray.length; i += 2){
+    for (let i = 1; i <= expressionArray.length; i += 2) {
         operators.push(expressionArray[i]);
     }
     operators.pop(); // gets rid of the extra operator at the end of the expression
 
     // reordering the operators according to BEDMAS
-    for (let i = 0; i < operators.length; i++){
-        if(operators[i] === '×' || operators[i] === '÷'){ // first we extract the multiplication and division operators
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] === '×' || operators[i] === '÷') { // first we extract the multiplication and division operators
             bedmas.push(operators[i]);
         }
     }
-    for(let i = 0; i < operators.length; i++){
-        if(operators[i] === '+' || operators[i] === '-'){ // and then we extract the addition and subtraction operators
+    for (let i = 0; i < operators.length; i++) {
+        if (operators[i] === '+' || operators[i] === '-') { // and then we extract the addition and subtraction operators
             bedmas.push(operators[i]);
         }
     }
 
     // grabbing all the operands in the expression and reordering them according to BEDMAS
-    for(let i=0; i < expressionArray.length; i+=2){
-        if(i==0){
+    for (let i = 0; i < expressionArray.length; i += 2) {
+        if (i == 0) {
             operands.push(expressionArray[i]);
         }
-        else if(i >= 2 && expressionArray[i - 1] === '×' || i >= 2 && expressionArray[i - 1] === '÷'){
+        else if (i >= 2 && expressionArray[i - 1] === '×' || i >= 2 && expressionArray[i - 1] === '÷') {
             operands.push(expressionArray[i]);
         }
     }
-    for(let i=0; i < expressionArray.length; i+=2){
-        if(i >= 2 && expressionArray[i - 1] === '+' || i >= 2 && expressionArray[i - 1] === '-'){
+    for (let i = 0; i < expressionArray.length; i += 2) {
+        if (i >= 2 && expressionArray[i - 1] === '+' || i >= 2 && expressionArray[i - 1] === '-') {
             operands.push(expressionArray[i]);
         }
     }
@@ -64,27 +65,42 @@ function levelTwo(){
     console.log(operands[0]);
 
     // calculating with BEDMAS to determine the final evaluation
-    for(let i = 0; i < bedmas.length; i++){
-        if(bedmas[i] === '×'){
+    for (let i = 0; i < bedmas.length; i++) {
+        if (bedmas[i] === '×') {
             evaluation *= operands[i + 1];
         }
-        else if(bedmas[i] === '÷'){
+        else if (bedmas[i] === '÷') {
             evaluation /= operands[i + 1];
         }
-        else if(bedmas[i] === '+'){
+        else if (bedmas[i] === '+') {
             evaluation += operands[i + 1];
         }
-        else if(bedmas[i] === '-'){
+        else if (bedmas[i] === '-') {
             evaluation -= operands[i + 1];
         }
         evaluation = Math.round(evaluation);
     }
-    
+
     console.log(expressionArray);
     console.log(operators);
     console.log(operands);
     console.log(bedmas);
     console.log(evaluation);
+
+    // configuring the HTML elements
+    for (let i = 0; i < expressionArray.length; i++) {
+        if (typeof expressionArray[i] === "number") {
+            let dice = document.createElement('img');
+            dice.src = `../images/dice-${expressionArray[i]}.png`;
+            gameScreen.append(dice);
+        }
+        else {
+            let operator = document.createElement('h1');
+            operator.innerHTML = expressionArray[i];
+            gameScreen.append(operator);
+        }
+    }
+
 
     // set up expression on screen
 
@@ -187,7 +203,7 @@ function levelTwo(){
 //         number1 = Math.floor(Math.random() * answer);
 //         number2 = Math.floor(Math.random() * answer);
 //     }
-    
+
 //     if (option1Answer === answer){
 //         option2Answer = number1;
 //         option3Answer = number2;
