3d9027a46c0391f565cb9d2b0e6b122e9e50a4c4
farzana-moury <w0737626@myscc.ca>
2023-03-04 14:25:01 -0500

    level three: setup select dice display

--- resources/scripts/levelThree.js
+++ resources/scripts/levelThree.js
@@ -3,9 +3,18 @@ let memoryDice = [];
 
 // html elements
 let gameScreen = document.getElementById('game-screen');
-let dicePanel = document.getElementById('dice-select');
+let dicePanel = document.getElementById('select-dice');
 
 function levelThree() {
+
+    // populate the select dice panel
+    for (let i=1; i<=6; i++){
+        let dice = document.createElement('img');
+        dice.src = `../images/dice-${i}.png`;
+        dice.id = "" + i;
+        dicePanel.appendChild(dice);
+    }
+
     // generate three random dice and display to user
     for (let i=0; i<3; i++){
         let randomNum = Math.floor(Math.random() * 6) + 1;
