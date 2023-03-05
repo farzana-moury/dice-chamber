e849289e429ab2cc6dd62d89c90a1489a6e9a873
farzana-moury <w0737626@myscc.ca>
2023-03-05 18:40:56 -0500

    #2 level three: timing functionality needs work

--- resources/scripts/levelThree.js
+++ resources/scripts/levelThree.js
@@ -1,5 +1,6 @@
 // important variables
 let memoryDice = [], selectedDice = [];
+let winRound = 1, winLevel;
 
 // html elements
 let gameScreen = document.getElementById('game-screen');
@@ -26,25 +27,53 @@ function levelThree() {
         memoryDice.push(randomNum);
     }
     
-    setTimeout(generateMemoryDice, 3000);
+    generateMemoryDice();
 
     console.log(memoryDice);
 
+    // we need to determine which dice from the dice panel the user clicked on
     dicePanel.querySelectorAll('.selected-dice').forEach(dice => {
         dice.addEventListener('click', () => {
             selectedDice.push(Number(dice.id));
             console.log(selectedDice);
+
+            if(selectedDice[0] === memoryDice[0] && selectedDice[1] === memoryDice[1] && selectedDice[2] === memoryDice[2]){
+                winRound++;
+                memoryDice = [];
+                selectedDice = [];
+                gameScreen.innerHTML = '';
+                dicePanel.innerHTML = '';
+                levelThree();
+        
+                console.log(winRound);
+            } else {
+                console.log('not working');
+            }
         });
     })
 }
 
+// set timer for memory dice to display
 function generateMemoryDice(){
-    gameScreen.style.visibility = 'hidden';
-    setTimeout(displayDicePanel, 1000);    
+    if(gameScreen.style.visibility === 'hidden'){
+        setTimeout(() => {
+            gameScreen.style.visibility = 'visible';
+        }, 3000);
+    } else {
+        setTimeout(() => {
+            gameScreen.style.visibility = 'hidden';
+        }, 3000);
+    }  
+    displayDicePanel();  
 }
 
+// the dice panel is revealed, where the user tries to remember the memory dice
 function displayDicePanel(){
-    dicePanel.style.visibility = 'visible';
+    if(dicePanel.style.visibility === 'hidden'){
+        setTimeout(() => {
+            dicePanel.style.visibility = 'visible';
+        }, 3000);
+    } 
 }
 
 levelThree();
