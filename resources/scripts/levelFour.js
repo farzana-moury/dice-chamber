986594867eb4984bb177ec243e8732314e49bf5d
farzana-moury <w0737626@myscc.ca>
2023-03-05 22:07:40 -0500

    level four: configure level completion

--- resources/scripts/levelFour.js
+++ resources/scripts/levelFour.js
@@ -27,24 +27,37 @@ function levelFour() {
 
     console.log(correctGuess, placeholder);
 
+    // simulate the dice roll
     document.querySelectorAll('#click-dice').forEach(dice => {
         dice.addEventListener('click', () => {
             guess = Math.floor(Math.random() * 6) + 1;
             console.log(guess);
             dice.src = `../images/dice-${guess}.png`;
+
+            // if the user guesses right the label displays correct ...
             if (guess === correctGuess) {
                 guessLabel.innerHTML = 'CORRECT';
                 guessLabel.style.color = 'green';
                 setTimeout(() => {
+                    // and we progress to the next round
                     winRound++;
                     gameScreen.innerHTML = '';
                     guessLabel.innerText = '?';
                     levelFour();
                 }, 3000);
-            } else {
+            } else { // otherwise the label will tell you it's wrong
+                guessLabel.style.color = 'white';
                 guessLabel.innerHTML = 'Bad roll.';
             }
-            console.log('rounds: ' + winRound);
+            
+            // once 10 rounds are complete, the level is won
+            if (winRound === 10){
+                winLevel = true;
+            }
+            if (winLevel){
+                gameScreen.innerHTML = '';
+                window.location.href = "../levels/endScreen.html";
+            }
         });
     })
 }
