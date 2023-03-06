9cff90d3f48bf6b2c6777aa130df9b3878ada74f
farzana-moury <w0737626@myscc.ca>
2023-03-05 21:38:12 -0500

    level four: setup and test important variables

---
+++ resources/scripts/levelFour.js
@@ -0,0 +1,30 @@
+// important variables
+let guess, correctGuess;
+let winRound = 1, winLevel;
+
+// html elements
+let gameScreen = document.getElementById('game-screen');
+
+function levelFour(){
+    let correctGuess = Math.floor(Math.random() * 6) + 1; // generate a random roll number - this is the actual guess
+    let placeholder = Math.floor(Math.random() * 6) + 1; // generate placeholder number for dice clicker
+
+    // make sure the placeholder number is not the same as the actual guess number
+    while(correctGuess === placeholder){
+        placeholder = Math.floor(Math.random() * 6) + 1;
+    }
+
+    // adding the guess label - which displays if you have the right roll or not
+    let guessLabel = document.createElement('h2');
+    guessLabel.innerText = '?';
+    gameScreen.appendChild(guessLabel);
+
+    // adding the dice where the user will repeatedly click (i.e. roll)
+    let dice = document.createElement('img');
+    dice.src = `../images/dice-${placeholder}.png`;
+    gameScreen.appendChild(dice);
+
+    console.log(correctGuess, placeholder);
+}
+
+levelFour();
